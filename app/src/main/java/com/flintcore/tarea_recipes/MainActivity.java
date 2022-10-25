a741a033370804e0cf0cfac346c8458479f12202
LinkFalcon0921 <elioerickramos@gmail.com>
2022-10-25 16:16:49 -0400

    first commit

---
+++ app/src/main/java/com/flintcore/tarea_recipes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.flintcore.tarea_recipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
